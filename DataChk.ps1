c4c2f4b59a89c838c62ebbe12a4296838e1f18c2
Syuichi Murashima <mura@vwnet.jp>
2022-06-22 10:44:58 +0900

    ショートカット作るようにした

--- DataChk.ps1
+++ DataChk.ps1
@@ -284,7 +284,18 @@ $TergetDirectory = "C:\DataCheck"
 $PSFullName = Join-Path $TergetDirectory "DataChk.ps1"
 if( -not (Test-Path $TergetDirectory)){mkdir $TergetDirectory}
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/MuraAtVwnet/ystsp/master/DataChk.ps1 -OutFile $PSFullName
+
+# Create shortcut
+$WsShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WsShell.CreateShortcut("C:\Users\Public\Desktop\ORS DataCheck.lnk")
+$Shortcut.TargetPath = $TergetDirectory
+$Shortcut.Save()
+
+# Open directory
 Invoke-Item $TergetDirectory
+
+# Close Window
+exit
 
 # Execute Script
 . "C:\DataCheck\DataChk.ps1"
