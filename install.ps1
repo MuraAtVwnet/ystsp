9a6ff1d90ce313e2e7acd303103695435f66df0a
Syuichi Murashima <mura@vwnet.jp>
2022-06-23 15:32:31 +0900

    test

--- install.ps1
+++ install.ps1
@@ -1,20 +1,13 @@
-﻿# Pre Setting
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
-
-# Get Script
+﻿Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
 $TergetDirectory = "C:\DataCheck"
 $PSFullName = Join-Path $TergetDirectory "DataChk.ps1"
 if( -not (Test-Path $TergetDirectory)){mkdir $TergetDirectory}
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/MuraAtVwnet/ystsp/master/DataChk.ps1 -OutFile $PSFullName
-
-# Create shortcut
 $WsShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WsShell.CreateShortcut("C:\Users\Public\Desktop\ORS DataCheck.lnk")
 $Shortcut.TargetPath = $TergetDirectory
 $Shortcut.Save()
-
-# Open directory
 Invoke-Item $TergetDirectory
-
+clsoe
 
 # Invoke-Expression "& { $(Invoke-RestMethod https://raw.githubusercontent.com/MuraAtVwnet/ystsp/master/install.ps1) }"
