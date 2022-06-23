a65894a307c7ad9544438309b814be071235d68c
Syuichi Murashima <mura@vwnet.jp>
2022-06-23 15:26:11 +0900

    インストーラー作ってみた

---
+++ install.ps1
@@ -0,0 +1,18 @@
+﻿# Pre Setting
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+
+# Get Script
+$TergetDirectory = "C:\DataCheck"
+$PSFullName = Join-Path $TergetDirectory "DataChk.ps1"
+if( -not (Test-Path $TergetDirectory)){mkdir $TergetDirectory}
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/MuraAtVwnet/ystsp/master/DataChk.ps1 -OutFile $PSFullName
+
+# Create shortcut
+$WsShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WsShell.CreateShortcut("C:\Users\Public\Desktop\ORS DataCheck.lnk")
+$Shortcut.TargetPath = $TergetDirectory
+$Shortcut.Save()
+
+# Open directory
+Invoke-Item $TergetDirectory
+
